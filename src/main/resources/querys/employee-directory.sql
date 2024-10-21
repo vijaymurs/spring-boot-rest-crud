d8671ceecbde94b856eb5ff8caf7e015b5ece863
Vijayakumar Murugan <49067711+vijaymurs@users.noreply.github.com>
2024-10-21 19:51:09 +0530

    sql file update

---
+++ src/main/resources/querys/employee-directory.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `is_deleted` BOOLEAN DEFAULT TRUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `employee`
+--
+
+INSERT INTO `employee` VALUES 
+	(1,'vijay','murugan','vijay@gamil.com'),
+	(2,'jay','vijay','emma@gmail.com');
+
